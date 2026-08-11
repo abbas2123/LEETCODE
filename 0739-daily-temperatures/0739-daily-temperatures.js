2a339a8520e74e2454ae5ae0b9b425035b638f80
Muhammed Abbas <abbasakk398@gmail.com>
2026-08-12 00:26:41 +0530

    Time: 17 ms (60.5%), Space: 78.2 MB (78.24%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (temperatures) {
+
+    let arr = new Array(temperatures.length).fill(0);
+    let stack = [];
+
+    for(let i=0;i<temperatures.length;i++){
+        while(stack.length>0&&temperatures[i]>temperatures[stack[stack.length-1]]){
+            let previndex = stack.pop();
+
+            arr[previndex] = i-previndex;
+        }
+        stack.push(i);
+    }
+    return arr
+};
